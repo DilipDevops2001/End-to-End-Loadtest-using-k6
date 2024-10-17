1d7c8e62094a74f8ae60de25027092fb9973661f
DilipDevops2001 <palanidilip5511@gmail.com>
2024-10-17 16:16:31 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,16 @@
 variable "project_id" {
-  type = string
+  type        = string
+  description = "GCP Project ID"
 }
 
 variable "region" {
-  type = string
-  default = "us-central1"
-}
-
-variable "zones" {
-  type = list(string)
-  default = ["us-central1-a", "us-central1-b", "us-central1-c"]
+  type        = string
+  default     = "us-central1"
+  description = "Region for GCP resources"
 }
 
 variable "instance_count" {
-  type    = number
-  default = 3
+  type        = number
+  default     = 3
+  description = "Number of instances to create"
 }
