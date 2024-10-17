f52d37df881403b2a697a82e89417c8becddb58a
DilipDevops2001 <palanidilip5511@gmail.com>
2024-10-17 16:16:54 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,15 @@
-# Output the EC2 public IP addresses as a file for Ansible inventory
+# Output public IPs of instances
 output "instance_public_ips" {
-  value = aws_instance.k6_instance[*].public_ip
+  value = google_compute_instance.loadtest_instance[*].network_interface[0].access_config[0].nat_ip
 }
 
-# Write the inventory for Ansible
+# Generate Ansible inventory
 resource "local_file" "ansible_inventory" {
   content = <<EOT
 [loadtest_nodes]
-%{ for ip in aws_instance.k6_instance[*].public_ip ~}
-${ip} ansible_ssh_user=ec2-user ansible_ssh_private_key_file=~/.ssh/your_key.pem
+%{ for ip in google_compute_instance.loadtest_instance[*].network_interface[0].access_config[0].nat_ip ~}
+${ip} ansible_ssh_user=your-gcp-username ansible_ssh_private_key_file=~/.ssh/your_key.pem
 %{ endfor ~}
 EOT
-  filename = "${path.module}/../ansible/hosts"
+  filename = "${path.module}/../ansible/inventory.ini"
 }
